dfe4050ef4dc408efe83fbd95ce5fdf6419a0e2c
fcastautomation1-ux <fcast.automation1@gmail.com>
2026-09-07 03:30:48 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# --- Google Sheets ---
+SPREADSHEET_ID = '1cQu7YLQ6R0T-I2SmqaJJvoMemxCHbOQXISts6f8TNdg' 
+WORKSHEET_NAME = 'Ad Scraper'                   
+CREDENTIALS_FILE = 'creds.json'             
+
+# --- Scraper settings ---
+HEADLESS = False      
+
+WAIT_TIMEOUT = 20     # Seconds to wait for the video to load after clicking play
